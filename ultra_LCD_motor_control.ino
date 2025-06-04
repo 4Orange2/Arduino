131fde27685dd95d0b4241b48b2fbbb0d4e3987e
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2025-06-04 09:31:16 -0400

    Update ultra_LCD_motor_control.ino

--- ultra_LCD_motor_control.ino
+++ ultra_LCD_motor_control.ino
@@ -54,37 +54,70 @@ void loop() {
   Serial.begin(9600);
   } 
   else if (choice == "motor") {
-  Serial.println("Give Speed and then time of operation in seconds (separately): ");
-  Serial.end();
-  Serial.begin(9600);
-  while (Serial.available() == false) {} 
-  int speed = Serial.parseInt();
-  Serial.end();
-  Serial.begin(9600);
-  while (Serial.available() == false) {} 
-  int time_of_oper = Serial.parseInt();
-  Serial.println("Speed: ");
-  Serial.println(speed);
-  Serial.println("Time: ");
-  Serial.println(time_of_oper);  
-  // Goes at desired speed for time specified
-  if (speed >= 0 && speed <= 255) {
-    Serial.println("entered");
+    Serial.println("Give Speed and then time of operation in seconds (separately): ");
+    Serial.end();
+    Serial.begin(9600);
+    while (Serial.available() == false) {} 
+    int speed = Serial.parseInt();
+    Serial.end();
+    Serial.begin(9600);
+    while (Serial.available() == false) {} 
+    int time_of_oper = Serial.parseInt();
+    Serial.println("Speed: ");
+    Serial.println(speed);
+    Serial.println("Time: ");
+    Serial.println(time_of_oper);  
+    // Goes at desired speed for time specified
     init_time = millis();
-    bool stopped = false;
-    analogWrite(motorPin, speed);
-    while (stopped == false) {
-      Serial.println(millis());
-      Serial.println(init_time);
-      current_time = millis();
-      if ((current_time - init_time) >= (time_of_oper * 1000)) {
-        Serial.println("AHHAAAAAAAAH");
-        analogWrite(motorPin, 0);
-        stopped = true;
+    if (speed >= 0 && speed <= 255) {
+      bool stopped = false;
+      analogWrite(motorPin, speed);
+      while (stopped == false) {
+        current_time = millis();
+        String second_choice = Serial.readString();
+        Serial.println("This is second choice: ");
+        Serial.println(second_choice);
+        second_choice.trim();
+        delay(1000);
+        if (second_choice == "ultrasound") {
+          lcd.clear();
+          digitalWrite(trigPin, HIGH);
+          delayMicroseconds(10);
+          digitalWrite(trigPin, LOW);
+          int duration = pulseIn(echoPin, HIGH); 
+          // pulseIn() returns duration in microseconds
+          // Calculating the distance
+          int distance_in_cm = duration * 0.034 / 2;
+          // speed of sound = 340 m/s
+          // speed of sound/ms = 0.34 m/ms or 34 cm/ms microseconds
+          // Prints the distance on the Serial Monitor
+          int distance_in_m = distance_in_cm / 100;
+          Serial.print("Distance: ");
+          Serial.println(distance_in_cm);
+          Serial.println(distance_in_m);
+          lcd.setCursor(1, 0);
+          lcd.print("Distance: ");
+          lcd.setCursor(11, 0);
+          lcd.print(distance_in_cm);
+          lcd.setCursor(12 + String(distance_in_cm).length(), 0);
+          lcd.print("cm");
+          Serial.end();
+          Serial.begin(9600);
         }
+        else if (second_choice == "stop") {
+          Serial.println("ENTERED STOP");
+          analogWrite(motorPin, 0);
+          stopped = true;
+        }
+        if ((current_time - init_time) >= (time_of_oper * 1000)) {
+          Serial.println("AHHAAAAAAAAH");
+          Serial.println(choice);
+          analogWrite(motorPin, 0);
+          stopped = true;
+        }
+      }
     }
-  }
-  Serial.end();
-  Serial.begin(9600);
+    Serial.end();
+    Serial.begin(9600);
   }
 }
